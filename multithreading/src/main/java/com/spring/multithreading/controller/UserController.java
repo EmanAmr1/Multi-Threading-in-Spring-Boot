243cf884504ac4c67e6be065aaccdb2256cefcc0
EmanAmr1 <emanamr543@gmail.com>
2024-08-03 19:28:22 +0300

    Add asynchronous user retrieval endpoint with threading

--- multithreading/src/main/java/com/spring/multithreading/controller/UserController.java
+++ multithreading/src/main/java/com/spring/multithreading/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.spring.multithreading.controller;
 
+import com.spring.multithreading.entity.User;
 import com.spring.multithreading.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -35,4 +37,22 @@ public class UserController {
 
     }
 
+    @GetMapping(value = "/getUsersByThread", produces = "application/json") //It specifies that the response should be in JSON format.
+    public ResponseEntity getUsers(){
+        CompletableFuture<List<User>> users1 = userService.findAllUsers();
+        CompletableFuture<List<User>> users2 = userService.findAllUsers();
+        CompletableFuture<List<User>> users3 = userService.findAllUsers();
+        CompletableFuture.allOf(users1,users2,users3).join();
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    /*
+    allOf does not block the current thread.
+    It simply ensures that the combined CompletableFuture completes when all individual futures
+    (users1, users2, users3) complete.
+   join() is then called on this combined future to block the current thread
+   until all asynchronous operations (findAllUsers calls) are complete.
+    This ensures that the method waits for all user fetching operations to finish before proceeding.
+
+     */
 }
